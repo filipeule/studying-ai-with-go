72f5caa76b11f6a8c298e29d7c0279c7f20d694d
filipeule <filipe_ule@hotmail.com>
2026-01-12 18:39:02 -0300

    start cleaning room random algorithm

--- 02-ai-search-ii/random.go
+++ 02-ai-search-ii/random.go
@@ -6,8 +6,35 @@ func CleanRoomRandomWalk(room *Room, robot *Robot) {
 	startTime := time.Now()
 	moveCount := 0
 
-	// clean room
-	
+	// set variables
+	maxMoves := room.Width * room.Height * 5
+	stuckCount := 0
+	maxStuckCount := 5 // max number of consecutive failed moves before changing strategy
+
+	// clean current position
+	Clean(robot, room)
+	if room.Animate {
+		room.Display(robot, false)
+		time.Sleep(moveDelay)
+	}
+
+	for moveCount < maxMoves && room.CleanedCellCount < room.CleanableCellCount {
+		// generate a random angle in radians
+
+		// calculate a direction vector based on angle
+
+		// use bresenham's line algorithm to move in that direction until hitting an obstacle
+
+		// if we didnt move very much, increment stuck count and possibly change strategy
+
+		// if stuck too many time, use A* to find path to nearest dirty cell
+
+		// add some adaptive behaviour. scan for dirty cells every once in awhile
+
+		// end for
+
+		// final sweep to ensure complete coverage
+	}
 
 	// calculate cleaning time
 	cleaningTime := time.Since(startTime)
